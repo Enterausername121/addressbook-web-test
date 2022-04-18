f996fca56f955925207d0bd17ee3cb36db698fe5
Я <@DESKTOP-A4BA2S2>
2022-04-19 01:45:50 +0200

    lesson2.5

---
+++ addressbook-web-test/Tests/GroupModificationTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace addressbook_web_test.Tests
+{
+    [TestFixture]
+    public class GroupModificationTests : BaseTest
+    {
+        [Test]
+        public void GroupModificationTest()
+        {
+            GroupData newData = new GroupData("aer");
+            newData.Header = "aer";
+            newData.Footer = "aer";
+            applicationManager.Groups.Modify(1, newData);
+            applicationManager.Navigator.GoToGroupsPage();
+            applicationManager.Auth.Logout();
+        }
+
+    }
+}
