7ca19bb583396330a6b795b02bc48457e1e79584
arodionov <deadall2005@gmail.com>
2022-05-21 23:19:15 +0300

    3.4

---
+++ addressbook-web-test/Tests/AuthBaseTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace addressbook_web_test
+{
+    public class AuthBaseTest : BaseTest
+    {
+        [SetUp]
+        public void SetupLogin()
+        { 
+            applicationManager.Auth.Login(new AccountData("admin", "secret"));
+        }
+    }
+}
---
+++ addressbook-web-test/Tests/LoginTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+namespace addressbook_web_test
+{
+    [TestFixture]
+    public class LoginTest: BaseTest
+    {
+        [Test]
+        public void LoginWithInvalidCredentials()
+        {
+            // prepare
+            applicationManager.Auth.Logout();
+            // action
+            AccountData account = new AccountData("admin", "123456");
+            applicationManager.Auth.Login(account);
+            //verification
+            Assert.IsFalse(applicationManager.Auth.IsLoggedIn(account));
+        }
+
+        [Test]
+        public void LoginWithValidCredentials()
+        {
+            // prepare
+            applicationManager.Auth.Logout();
+            // action
+            AccountData account = new AccountData("admin", "secret");
+            applicationManager.Auth.Login(account);
+            //verification
+            Assert.IsTrue(applicationManager.Auth.IsLoggedIn(account));
+        }
+
+        
+    }
+}
